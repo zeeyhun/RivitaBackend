033c65d1a158657a9b29bd04c4f45b3451343ad5
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-10-23 17:01:58 +0300

    databas files

---
+++ RivitaBackend/Models/Transportation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RivitaBackend.Models
+{
+    public class Transportation
+    {
+    }
+}
